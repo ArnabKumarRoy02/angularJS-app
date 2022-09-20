c487878c637d2a57ca64ea771234a09a1ec5ea12
ArnabKumarRoy02 <arnabroy770@gmail.com>
2022-09-20 11:22:10 +0530

    update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,18 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'my-new-app';
+  todoItem: string = '';
+  
+  listofTodos: string[] = [];
+
+  addToTodolist = () => {
+    console.log("clicked")
+    this.listofTodos.push(this.todoItem);
+  }
+
+  deleteItem = (indexofTheItem: number) => {
+    console.log(indexofTheItem);
+    this.listofTodos.splice(indexofTheItem, 1);
+  }
+
 }
