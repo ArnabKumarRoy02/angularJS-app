b9ab8d8eda57e3e3c7f2fec9bc21fcc8dac767be
ArnabKumarRoy02 <arnabroy770@gmail.com>
2022-09-27 10:29:57 +0530

    update

--- src/app/todo/todo.component.ts
+++ src/app/todo/todo.component.ts
@@ -8,6 +8,15 @@ import { Component, OnInit } from '@angular/core';
 export class TodoComponent implements OnInit {
 
   constructor() { }
+  username: string = "hello";
+
+  get userName(): string {
+    return this.username;
+  }
+
+  set userName(val: string) {
+    this.username = val;
+  }
 
   ngOnInit(): void {
   }
